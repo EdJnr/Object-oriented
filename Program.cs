4db94f34dc616a09bf199dae904514eb0c34af82
EdJnr <edwardabladei@gmail.com>
2023-06-07 00:17:36 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var account1 = new Bank("Edward Nii Amarh Amartey Junior", 100);
+            Console.WriteLine($"New account created for {account1.Owner} with amount of {account1.Balance} and account number {account1.Number}");
+
+            account1.MakeDeposit(1000, DateTime.Now, "Deposit at Teshie Branch");
+            account1.MakeWithdrawal(100, DateTime.Now, "Withdrawal at Teshie Branch");
+            Console.WriteLine($"Current balance is {account1.Balance} cedis");
+            Console.WriteLine(account1.GetAccountHistory());
+
+
+        }
+    }
+}
---
+++ oop.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Bank
+    {
+        public string Number { get; } 
+        public string Owner { get; set; }
+
+        public decimal Balance { get 
+            {
+                decimal balance = 0;
+                foreach (var item in Transactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            } 
+        }
+
+        private static int AccountSeed = 140;
+
+        private List<transaction> Transactions = new List<transaction>();
+       
+        public Bank(string Owner,decimal balance)
+        {
+            this.Owner = Owner;
+            this.Number = $"CAL{AccountSeed++}";
+
+            //make initial deposit
+            MakeDeposit(balance, DateTime.Now, $"Initial deposit for {Owner}");
+        }
+
+        public void MakeDeposit(decimal amount,DateTime date,string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount to be deposited must be positive");
+            }
+
+            var withdrawal = new transaction(amount, date, note); 
+            Transactions.Add(withdrawal);
+        } 
+        
+        
+        public void MakeWithdrawal(decimal amount,DateTime date,string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount to be withdrawn must be not be less than zero");
+            }
+            if ((Balance - amount) < 0)
+            {
+                throw new InvalidOperationException("Not enough funds to perform this operation");
+            }
+
+            var withdrawal = new transaction(-amount, date, note);
+            Transactions.Add(withdrawal);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            report.AppendLine("Date\t\tAmount\t\tNote");
+            foreach (var item in Transactions)
+            {
+                report.AppendLine($"{item.Date}\t\t{item.Amount}\t\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+    }
+}
